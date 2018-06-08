c179d6702dfb40cf4c6418116c63952082b39705
Matt Kehrls <matthew.kehrls@gmail.com>
2018-06-08 17:30:38 -0400

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -2,33 +2,27 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to SERVER!');
-
-    // socket.emit('createMessage', {
-    //     from: 'Mark',
-    //     text: 'Hey. Its Mark.'
-    // });
 });
-
 
 socket.on('disconnect', function () {
     console.log('Disconnected from SERVER!');
 });
 
 socket.on('newMessage', function (message) {
-    var formattedTime = moment(message.createdAt).format('h:mm a')
-    console.log("New Message!" , message);
-    var li = $('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    $('#messages').append(li);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+    $('#messages').append(html);
+
+    // console.log("New Message!" , message);
+    // var li = $('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    // $('#messages').append(li);
 });
-
-// socket.on('welcomeMessage', function (message) {
-//     console.log(message.text);
-// });
-
-// socket.on('joinMessage', function (message) {
-//     console.log(message.text);
-// });
 
 // socket.emit('createMessage', {
 //     from : 'Frank',
@@ -38,17 +32,24 @@ socket.on('newMessage', function (message) {
 // });
 
 socket.on('newLocationMessage', function (message) {
-    var formattedTime = moment(message.createdAt).format('h:mm a')
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My current location</a>');
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var locTemplate = $('#location-message-template').html();
+    var html = Mustache.render(locTemplate, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+    $('#messages').append(html);
+    
+    // var formattedTime = moment(message.createdAt).format('h:mm a')
+    // var li = $('<li></li>');
+    // var a = $('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from} ${formattedTime} : `);
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
-
+    // li.text(`${message.from} ${formattedTime} : `);
+    // a.attr('href', message.url);
+    // li.append(a);
+    // $('#messages').append(li);
 });
-
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
@@ -63,23 +64,18 @@ jQuery('#message-form').on('submit', function (e) {
     });
 });
 
-
 var locationButton = $('#send-location');
-
 locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert("Geo-Location is NOt supported by your browser!");
-    } 
-
+    }
     locationButton.attr('disabled', 'disabled').text("Sending Location..!");
-
     navigator.geolocation.getCurrentPosition(function (position) {
         locationButton.removeAttr('disabled').text("Send Location");
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
-
     }, function () {
         locationButton.removeAttr('disabled').text("Sending Location");
         alert('Unable to fetch location');
